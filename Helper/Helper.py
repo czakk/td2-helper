3a7a10fb675b468c6c529f8c767ce2dc2401ea3e
czakk <czakcbprogramming@gmail.com>
2020-11-05 17:47:55 +0100

    Update Helper.py

--- Helper/Helper.py
+++ Helper/Helper.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 print("Witaj w Generatorze komend!")
 print("Podaj co chcesz spawnować: .")
 wagon = input("Podaj rodzaj wagonu: ")
@@ -11,6 +11,4 @@ print("Komenda:\n/sp",sem+":"+distance,"n:"+amount+","+wagon)
 #/sp A n:5,eaos;
 # /sp [sygnalizator]:[odległość] [skład]
 #/rmcar Ps_It2e
-=======
 
->>>>>>> 82f1bc4f79e1ea92d936372e3b8f974387a8839c
