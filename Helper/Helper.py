e0e5df6300a6febe88292074abab443c238dab51
Cezary B <czakcbprogramming@gmail.com>
2020-11-05 20:10:35 +0100

    pre-version

--- Helper/Helper.py
+++ Helper/Helper.py
@@ -1,11 +1,40 @@
-print("Witaj w Generatorze komend!")
-print("Podaj co chcesz spawnować: .")
-wagon = input("Podaj rodzaj wagonu: ")
-sem = input("Podaj semafor pod którym spawnuja sie skład: ") #Podajemy Semafor
-distance = input("Podaj odległość od semafora: ") #Odległość od semafora
-amount = input("Podaj ilość wagonów: ") #Ilość wagonów
+# github.com/czakk; author: czak; date:05/11/2020
+#Title: TD2 Helper;
+#Description: Generator komend do symulatora TD2(td2.info.pl);version: 0.1;Last-Update: 05/11/2020;
 
-print("Komenda:\n/sp",sem+":"+distance,"n:"+amount+","+wagon)
+print("""Witam w Generatorze Komend w symulatorze Train Driver 2\t
+            Jest to wersja wczesna, błędy można zgłaszać na forum,github,dc:czak#4333\t\t
+                Dziękuje za testowanie aplikacji\n""")
+
+
+menu_choice = None
+
+while menu_choice == None:
+    print("""Wybierz opcję która cię interesuję:
+            1.Generator składów
+            2.Wyrzucenia Gracza
+            3.Zamknięcie programu
+                                """)
+    menu_choice = input("Podaj numer: ")
+    if menu_choice:
+        if menu_choice == "1":
+            print("Witaj, w generatorze komeny do spawnów składów")
+            distance = "1"
+            #tu będzie pętla while sprawdzająca prawdziwość argumentów
+            post = input("Podaj Posterunek na którym się znajdujesz (np.Ps,Tw): ") #Podajemy posterunek na którym się znajdujemy, ponieważ wymagane jest podanie również tego przy zdefiniowaniu konkretnego semafora
+            sem = input("Podaj Posterunek na którym się znajdujesz (np.G2,A): ")
+            distance = input("Podaj dystans od semafora. Standardowa wartość to '1': ")
+            amount = input("Podaj ilość wagonów(min. 1): ")
+            wagoons = input("Jaki model konstrukcyjny chcesz spawnować: ")
+           
+            print("Wygenerowana komenda:")
+            print("\t/sp "+post.title()+"_"+sem.title()+":"+distance,"n:"+amount+","+wagoons)
+        else:print("Wkrótce!\n");menu_choice = None;continue
+
+           
+
+
+
 
 #/sp A n:5,eaos;
 # /sp [sygnalizator]:[odległość] [skład]
