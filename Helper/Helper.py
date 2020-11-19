6441a68406523e9f455e8dfa6b1a72359a54aa82
Cezary B <czakcbprogramming@gmail.com>
2020-11-19 23:55:06 +0100

    test file class added

--- Helper/Helper.py
+++ Helper/Helper.py
@@ -1,32 +1,57 @@
 #   github.com/czakk/td2-helper; author: czak; date:05/11/2020
 #   Title: TD2 Helper;
-#   Description: Generator komend do symulatora TD2(td2.info.pl);version: 0.4;Last-Update: 15/11/2020 by czak;
-def file_exist(file_path):
-    """Sprawdza czy podany plik istnieje!"""
-    try:
-        x = open(file_path)
-    except IOError:
-        verify = "brak połączenia z plikiem "+file_path
-        return verify
-    else:
-        verify = "zainicjowano plik "+file_path
-        x.close()
-        return verify
+#   Description: Generator komend do symulatora TD2(td2.info.pl);version: 0.5;Last-Update: 19/11/2020 by czak;
+
+class File_test(object):
+    """Testuje pliki czy posiadają poprawne parametry"""
+    
+    def __init__(self,file):
+        self.file = file
+        self.__error = None
+        try:
+            self.file = open(file,"r")
+            self.file_name = file
+        except IOError as e:
+            self.__error = e
+
+    def file_exist(self):
+        if self.__error:
+            print("Plik", self.file_name, "nie może zostać zainicjowany. Kod błędu",self.__error+"\n")
+            self.file = 0
+        else:
+            print("Plik", self.file_name,"został zainicjowany \n")
+       
+
+    def list_display(self):
+        """Usuwa wszystko po znaku '-' we wczytanym pliku"""
+        if self.file == 0:
+            print("Wystąpił problem z plikiem",self.file,"kod błędu",self.error)
+        else:
+            display_list = self.file.readlines()
+            
+            for i in range(len(display_list)):
+                display_list.insert(i,display_list[i].replace("\n",""))
+                display_list.pop(i+1)
+            
+            choice_list = display_list[:]
+            
+            for i in range(len(choice_list)):
+                choice_list.insert(i,choice_list[i][:choice_list[i].index("-")-1])
+                choice_list.pop(i+1)
+            return display_list,choice_list
 
 def load_vehicle(file_path):
     """Wczytuje liste pojazdów i przypisuje je do listy veh"""
-    file = open(file_path,"r")
-    vehicles = file.readlines()
+    file = File_test("squad.txt")
+    vehicles = file.list_display
     title = []
-    def test2(list1,list2,name):
+    def titles(list1,list2):
        """Usuwa title z pliku squad.txt"""
-       try: 
-           list1.append(list2.pop(list2.index(name+"\n")))
-       except ValueError:
-           print("nazwa",name,"nie istnieje")
-    test2(title,vehicles,"Wagony")
-    test2(title,vehicles,"Lokomotywy")
-    test2(title,vehicles,"EZT")
+       for i in list1:
+          if i[0] == "<":
+              list2.append(i[1:])
+              list1.pop(list1.index(i))
+    titles(vehicles,title)
     veh = []
     for i in range (len(title)):
         veh.append([])
@@ -37,24 +62,25 @@ def load_vehicle(file_path):
     file.close()
     return veh,title
 
-def load_post(file_path):
-    """Funkcja załadowuje posterunki z pliku podanego w argumencie, tworzy kopie listy posterunków i w tej kopii tworzy posterunki bez cały nazw np. Ps zamiast Ps - Piaskowo"""
-    file = open(file_path, "r")
-    post_todisplay = file.readlines()
-    for i in range(len(post_todisplay)):
-            post_todisplay.insert(i,post_todisplay[i].replace("\n",""))
-            post_todisplay.pop(i+1)
-    post_tochoice = post_todisplay[:]
-    for i in range(len(post_tochoice)):
-        post_tochoice.insert(i,post_tochoice[i][:post_tochoice[i].index("-")-1])
-        post_tochoice.pop(i+1)
-    return post_todisplay,post_tochoice
+#def load_post(file_path):
+#    """Funkcja załadowuje posterunki z pliku podanego w argumencie, tworzy kopie listy posterunków i w tej kopii tworzy posterunki bez cały nazw np. Ps zamiast Ps - Piaskowo"""
+#    file = open(file_path, "r")
+#    post_todisplay = file.readlines()
+#    for i in range(len(post_todisplay)):
+#            post_todisplay.insert(i,post_todisplay[i].replace("\n",""))
+#            post_todisplay.pop(i+1)
+#    post_tochoice = post_todisplay[:]
+#    for i in range(len(post_tochoice)):
+#        post_tochoice.insert(i,post_tochoice[i][:post_tochoice[i].index("-")-1])
+#        post_tochoice.pop(i+1)
+#    return post_todisplay,post_tochoice
 
 def choice_post():
-    display,choice = load_post("post.txt")
-    for i in range(len(display)):
+     file = File_test("post.txt")
+     display,choice = file.list_display()
+     for i in range(len(display)):
         print(str(i)+".",display[i])
-    while True:
+     while True:
         x = get_params("Który posterunek wybierasz?: ",1)
         try:
             return(choice[int(x)])
@@ -83,11 +109,13 @@ def choice_vehicle():
 
 def display_hello():
     """Wyświetlanie wiadomości powitalnej"""
+    f1 = File_test("squad.txt")
+    f2 = File_test("post.txt")
     print("""Witam w Generatorze Komend w symulatorze Train Driver 2\t
             Jest to wersja wczesna, błędy można zgłaszać na forum,github,dc:czak#4333\t\t
                 Dziękuje za testowanie aplikacji\n""")
-    print("Inicjacja pliku tekstowego:",file_exist("squad.txt"))
-    print("Inicjacja pliku tekstowego:",file_exist("post.txt"),"\n")
+    f1.file_exist()
+    f2.file_exist()
 
 def menu():
     """Menu wyboru między opcjami programu, zwraca nr. opcji"""
@@ -104,7 +132,6 @@ def menu():
               print("Nie ma takiej opcji!")
     return user_choice
 
-
 def get_params(question,req):
     """Zczytuje parametry i przypisuje je do zmiennej"""
     #req - czy wymagane jest podanie odpowiedzi
@@ -114,10 +141,9 @@ def get_params(question,req):
         variable = ""
         while len(variable) == 0:
             variable = input(question)
-
     return variable
 
-def sq_generator(choice,parms):
+def sq_generator(parms):
     """Generuje komendy do spwanowania składu, zwraca: """
     parms[0] = get_params("Podaj posterunek na którym się znajdujesz. Niektóre scenerie tego nie wymagają! (Cis). By otworzyc listę posterunków wpisz 'lista': ",0)
     if parms[0] == "lista":
@@ -129,10 +155,26 @@ def sq_generator(choice,parms):
     parms[3] = get_params("Podaj ilość wagonów: ",1)
     parms[4] = get_params("Jaki typ pojazdu chcesz stworzyć? Jeżeli chcesz otworzyć listę pojazdów wpisz 'lista': ",1)
     if parms[4] == "lista":
-       parms[4] = choice_vehicle()
+        parms[4] = choice_vehicle()
+
+    while True:
+        choice = get_params("Czy chcesz coś dodać do obecnego składu? t/n: ",1)
+        if choice.lower() == "t":
+            for i in range(2):
+                parms[5] = ""
+                if i == 0:
+                    parms[5] = get_params("Podaj ilość wagonów: ",1)
+                    parms[4] += ";n:"+parms[5]+","
+                else:
+                    parms[5] = get_params("Jaki typ pojazdu chcesz stworzyć? Jeżeli chcesz otworzyć listę pojazdów wpisz 'lista': ",1)
+                    if parms[5] == "lista":
+                        parms[5] = choice_vehicle()
+                    parms[4] += parms[5]
+        elif choice.lower() == "n":
+            break
     return parms
 
-def kick_generator(choice,parms):
+def kick_generator(parms):
     """Generuje komende która wyrzuca gracza"""
     parms[0] = get_params("Podaj nick lub indetyfikator gracza: ",1)
     parms[1] = get_params("Podaj powód (niewymagane): ",0)
@@ -147,15 +189,15 @@ def cmd_generator(parms,choice):
     else:print("Wystąpił Błąd")
 def main():
     """Główna funkcja programu"""
-    parms = [1,2,3,4,5]
+    parms = [1,2,3,4,5,""]
     display_hello()
     choice = menu()
     if choice == "1":
-      parms = sq_generator(choice,parms)
+      parms = sq_generator(parms)
     elif choice == "2":
-       parms = kick_generator(choice,parms)
+       parms = kick_generator(parms)
     cmd_generator(parms,choice)
-
+        
 main()
 
 #input("\nNaciśnij enter aby zakończyć!\n")
